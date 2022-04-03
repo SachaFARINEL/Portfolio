ba10c03bb8f257636eb8fe3f0e38e4d6438a2259
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-04-03 16:13:31 +0200

    hover doc cards

--- src/components/documentations.js
+++ src/components/documentations.js
@@ -6,23 +6,35 @@ const Documentations = () => {
     return (
         <>
             <Grid container justifyContent='center'>
-                <Grid item xs={2} >
-                    <div className='divProjets' style={{ width: '20vh', height: '20vh', overflow: 'hidden', borderRadius: '2vh', background: '#E1DBFF' }}>
-                        <img style={{ width: '100%' }} src='assets/nas3.svg'></img>
+                <Grid item xs={2} style={{ display: 'flex', justifyContent: 'center' }} >
+
+                    <div className='containerDoc' >
+                        <div className='transitionGrowth' style={{ width: '20vh', height: '20vh', overflow: 'hidden', borderRadius: '2vh', background: '#E1DBFF' }}>
+                            <img style={{ width: '100%' }} src='assets/nas3.svg'></img>
+                        </div>
+                        <figcaption className='descriptionDoc' style={{ fontSize: '' }}>Configuration d'un NAS</figcaption>
                     </div>
                 </Grid>
 
-                <Grid item xs={2} >
-                    <div className='divProjets' style={{ width: '20vh', height: '20vh', overflow: 'hidden', borderRadius: '2vh', background: '#FDE7E9' }}>
-                        <img style={{ width: '100%' }} src='assets/cloud3.svg'></img>
+                <Grid item xs={2} style={{ display: 'flex', justifyContent: 'center' }} >
+
+                    <div className='containerDoc' onClick={() => console.log('test')} >
+                        <div className='transitionGrowth' style={{ width: '20vh', height: '20vh', overflow: 'hidden', borderRadius: '2vh', background: '#FDE7E9' }}>
+                            <img style={{ width: '100%' }} src='assets/cloud3.svg'></img>
+                        </div>
+                        <figcaption className='descriptionDoc' style={{ fontSize: '' }}>Installation de Nextcloud</figcaption>
                     </div>
 
                 </Grid>
 
-                <Grid item xs={2} >
-                    <div className='divProjets' style={{ width: '20vh', height: '20vh', overflow: 'hidden', borderRadius: '2vh', background: '#CCF2FF' }}>
-                        <img style={{ width: '100%' }} src='assets/mitm3.svg'></img>
+                <Grid item xs={2} style={{ display: 'flex', justifyContent: 'center' }}>
+                    <div className='containerDoc' >
+                        <div className='transitionGrowth' style={{ width: '20vh', height: '20vh', overflow: 'hidden', borderRadius: '2vh', background: '#CCF2FF' }}>
+                            <img style={{ width: '100%' }} src='assets/mitm3.svg'></img>
+                        </div>
+                        <figcaption className='descriptionDoc'>Les attaques Man In The Middle</figcaption>
                     </div>
+
                 </Grid>
 
             </Grid>
