9d0911f4a23adb2601b173b42c05e830edcc348a
Sacha FARINEL <farinel.sacha@tutanota.com>
2022-04-23 13:58:40 +0200

    correction height img

--- src/components/projets.js
+++ src/components/projets.js
@@ -21,7 +21,7 @@ const Projets = (props) => {
                                         <Grid item xs={7} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
                                             <img style={{ width: '100%', paddingRight: '1rem' }} src='/assets/portail.svg'></img>
                                         </Grid>
-                                        <Grid item xs={5} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                                        <Grid item xs={5} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', maxHeight: "8rem" }}>
                                             <h1 style={{ fontFamily: 'Inter', color: '#998282', fontSize: '1.2rem', textAlign: 'center' }}>Calculateur de cotes</h1>
                                             <span className='descriptionProjet' style={{ textAlign: 'center', fontFamily: 'Inter', fontSize: '0.9rem' }}>Projet de stage réalisé avec <br /><span style={{ color: '#5ED3F3', fontWeight: '600' }}>React JS </span>et <span style={{ color: '#007AC1', fontWeight: '600' }}>Material UI </span></span>
                                         </Grid>
@@ -39,7 +39,7 @@ const Projets = (props) => {
                             <Card>
                                 <CardContent style={{ background: '#FAFAFF', maxHeight: '10rem' }}>
                                     <Grid container>
-                                        <Grid item xs={5} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                                        <Grid item xs={5} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', maxHeight: "8rem" }}>
                                             <h1 style={{ fontFamily: 'Inter', color: '#E8505B', fontSize: '1.2rem', textAlign: 'center' }}>Gestionnaire de rendez-vous</h1>
                                             <span className='descriptionProjet' style={{ textAlign: 'center', fontFamily: 'Inter', fontSize: '0.9rem' }}>Projet de fin d'études réalisé avec <br /><span style={{ color: '#7377AD', fontWeight: '600' }}>PHP </span>et <span style={{ color: '#1064A5', fontWeight: '600' }}>jQuery </span></span>
                                         </Grid>
@@ -65,7 +65,7 @@ const Projets = (props) => {
                                             <img style={{ width: '60%' }} src='/assets/staggers.svg'></img>
 
                                         </Grid>
-                                        <Grid item xs={5} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                                        <Grid item xs={5} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', maxHeight: "8rem" }}>
                                             <h1 style={{ fontFamily: 'Inter', color: '#2D2442', fontSize: '1.2rem' }}>Adopte un stagiaire</h1>
                                             <span className='descriptionProjet' style={{ textAlign: 'center', fontFamily: 'Inter', fontSize: '0.9rem' }}>Projet de fin d'études réalisé avec <br /><span style={{ color: '#E11E23', fontWeight: '600' }}>Java </span>et <span style={{ color: '#DB6900', fontWeight: '600' }}>JavaFX </span></span>
                                         </Grid>
